67f451d8916570140dcd1f644e0d77e6e69b48a5
ilaydasila <sila.kucukoz@gmail.com>
2020-07-20 12:38:36 +0200

    card memory game

---
+++ Road/DAY15-21/MemoryCardGame/scripts.js
@@ -0,0 +1,57 @@
+const cards = document.querySelectorAll(".memory-card");
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.toggle("flip");
+
+  if (!hasFlippedCard) {
+    //fist click
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+  //second click
+  secondCard = this;
+
+  checkMatch();
+}
+
+function checkMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetBoard();
+  }, 1000);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomNum = Math.floor(Math.random() * 12);
+    card.style.order = randomNum;
+  });
+})();
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
