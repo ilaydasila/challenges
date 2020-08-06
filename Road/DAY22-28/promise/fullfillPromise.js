59973bc1da472feb5f17a9dce77e7c2f718807ed
Ilayda Sıla Kucukoz <sila.kucukoz@gmail.com>
2020-08-06 14:05:12 +0200

    naming

--- Road/DAY22-28/promise/fullfillPromise.js
+++ Road/DAY22-28/promise/fullfillPromise.js
@@ -1,6 +1,6 @@
 ('use strict');
 
-var promise = new Promise(function (resolve, reject) {
+const promise = new Promise(function (resolve, reject) {
   setTimeout(() => resolve('FULFILLED!'), 300);
 });
 promise.then((value) => console.log(value));
